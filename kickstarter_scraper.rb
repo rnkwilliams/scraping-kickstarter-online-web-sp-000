38205fad74a3fa73d7b2e66df1146d985aa8d5ba
Ranika Williams <rnkprice07@gmail.com>
2019-07-06 23:46:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
 
 # projects: kickstarter.css("li.project.grid_4")
