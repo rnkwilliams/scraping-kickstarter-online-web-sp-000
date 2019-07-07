37434f04ee781ebe4b017842e232e656363686d2
Ranika Williams <rnkprice07@gmail.com>
2019-07-07 00:11:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,6 +12,6 @@ end
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta li a").text
+# location: project.css("ul.project-meta span.location-name").text
 # percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 create_project_hash
